c49b9b90c61e48a08d24c3b5f8f84ab464719025
Sarai <sarai.olcina.g@gmail.com>
2021-02-14 11:31:57 +0100

    Master branch

--- src/DateAndTime.java
+++ src/DateAndTime.java
@@ -2,9 +2,9 @@ public class DateAndTime {
     public static void main(String [] args){
         //Date and Time data types.
 
-        System.out.println("test 2");
+        System.out.println("On master branch");
 
-        System.out.println("on the 13th of February 2021");
+        //System.out.println("on the 13th of February 2021");
     }
 
 }
